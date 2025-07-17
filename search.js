fd9c055db23494d0461e0993815acaaca670363c
kavya1280 <kacrylic02@gmail.com>
2025-07-17 17:46:55 +0530

    the space between the search and filter is been cut

--- search.js
+++ search.js
@@ -47,23 +47,89 @@ document.addEventListener('DOMContentLoaded', async function () {
         filteredData.forEach((item)=>{
             const row = document.createElement("tr")
             row.innerHTML = `
-                <td>${item["Employee ID"]}</td>
-                <td>${item["Employee"]}</td>
-                <td>${item["Report Name"]}</td>
-                <td>${item["Expense Type"]}</td>
-                <td>${item["Report ID"]}</td>
-                <td>${item["Approval Status"]}</td>
-                <td>${item["Payment Status"]}</td>
-                <td>${item["Report Date"]}</td>
-                <td>${item["Transaction Date"]}</td>
-                <td>${item["Total Approved Amount"]}</td>
-                <td>${item["City/Location"]}</td>
-                <td>${item["Payment Type"]}</td>
-                <td>${item["Approved Amount"]}</td>
-                <td>${item["Submit Date"]}</td>
-                <td>${item["Person Band before PMS"]}</td>
-                <td>${item["Location"]}</td>
-                <td>${item["Country/Region"]}</td>
+        <td>${item["Employee"]}</td>
+        <td>${item["Employee ID"]}</td>
+        <td>${item["Report Name"]}</td>
+        <td>${item["Expense Type"]}</td>
+        <td>${item["Report ID"]}</td>
+        <td>${item["Approval Status"]}</td>
+        <td>${item["Payment Status"]}</td>
+        <td>${item["Report Date"]}</td>
+        <td>${item["Transaction Date"]}</td>
+        <td>${item["Total Approved Amount"]}</td>
+        <td>${item["City/Location"]}</td>
+        <td>${item["Payment Type"]}</td>
+        <td>${item["Approved Amount"]}</td>
+        <td>${item["Submit Date"]}</td>
+        <td>${item["Employee Name"]}</td>
+        <td>${item["Report Start Date"]}</td>
+        <td>${item["Report End Date"]}</td>
+        <td>${item["Currency"]}</td>
+        <td>${item["Report Total"]}</td>
+        <td>${item["Receipt Status"]}</td>
+        <td>${item["Cash Advance Return Received"]}</td>
+        <td>${item["Amount Due Employee"]}</td>
+        <td>${item["Report Id"]}</td>
+        <td>${item["Approved by Delegate"]}</td>
+        <td>${item["Exception Approved"]}</td>
+        <td>${item["Expense Report Auditing Status"]}</td>
+        <td>${item["Has Cleared Exceptions"]}</td>
+        <td>${item["Person Band before PMS"]}</td>
+        <td>${item["Business Unit"]}</td>
+        <td>${item["Location"]}</td>
+        <td>${item["Gender"]}</td>
+        <td>${item["Logon ID"]}</td>
+        <td>${item["Email Address"]}</td>
+        <td>${item["Country/Region"]}</td>
+        <td>${item["Travel Rule Class"]}</td>
+        <td>${item["Employee_Manager"]}</td>
+        <td>${item["Employee ID_Manager"]}</td>
+        <td>${item["Report Name_Manager"]}</td>
+        <td>${item["Expense Type_Manager"]}</td>
+        <td>${item["Report ID_Manager"]}</td>
+        <td>${item["Approval Status_Manager"]}</td>
+        <td>${item["Payment Status_Manager"]}</td>
+        <td>${item["Report Date_Manager"]}</td>
+        <td>${item["Transaction Date_Manager"]}</td>
+        <td>${item["Total Approved Amount_Manager"]}</td>
+        <td>${item["City/Location_Manager"]}</td>
+        <td>${item["Payment Type_Manager"]}</td>
+        <td>${item["Approved Amount_Manager"]}</td>
+        <td>${item["Submit Date_Manager"]}</td>
+        <td>${item["Report Start Date_Manager"]}</td>
+        <td>${item["Report End Date_Manager"]}</td>
+        <td>${item["Currency_Manager"]}</td>
+        <td>${item["Report Total_Manager"]}</td>
+        <td>${item["Receipt Status_Manager"]}</td>
+        <td>${item["Cash Advance Return Received_Manager"]}</td>
+        <td>${item["Amount Due Employee_Manager"]}</td>
+        <td>${item["Report Id_Manager"]}</td>
+        <td>${item["Approved by Delegate_Manager"]}</td>
+        <td>${item["Exception Approved_Manager"]}</td>
+        <td>${item["Expense Report Auditing Status_Manager"]}</td>
+        <td>${item["Has Cleared Exceptions_Manager"]}</td>
+        <td>${item["Person Band before PMS_Manager"]}</td>
+        <td>${item["Business Unit_Manager"]}</td>
+        <td>${item["Location_Manager"]}</td>
+        <td>${item["Gender_Manager"]}</td>
+        <td>${item["Logon ID_Manager"]}</td>
+        <td>${item["Email Address_Manager"]}</td>
+        <td>${item["Country/Region_Manager"]}</td>
+        <td>${item["Default Expense Report Approver_Manager"]}</td>
+        <td>${item["Default Expense Report Approver ID_Manager"]}</td>
+        <td>${item["Default Cash Advance Approver_Manager"]}</td>
+        <td>${item["Default Cash Advance Approver ID_Manager"]}</td>
+        <td>${item["Default Authorization Request Approver_Manager"]}</td>
+        <td>${item["Default Authorization Request Approver ID_Manager"]}</td>
+        <td>${item["Default Travel Request Approver_Manager"]}</td>
+        <td>${item["Default Travel Request Approver ID_Manager"]}</td>
+        <td>${item["BI Manager_Manager"]}</td>
+        <td>${item["BI Manager ID_Manager"]}</td>
+        <td>${item["Travel Employee Manager_Manager"]}</td>
+        <td>${item["Travel Employee Manager ID_Manager"]}</td>
+        <td>${item["Travel Rule Class_Manager"]}</td>
+        <td>${item["Difference"]}</td>
+
             `
             searchTableBody.appendChild(row)
         })
@@ -77,23 +143,88 @@ document.addEventListener('DOMContentLoaded', async function () {
     data.forEach((item) => {
         const row = document.createElement("tr")
         row.innerHTML = `
-            <td>${item["Employee ID"]}</td>
-            <td>${item["Employee"]}</td>
-            <td>${item["Report Name"]}</td>
-            <td>${item["Expense Type"]}</td>
-            <td>${item["Report ID"]}</td>
-            <td>${item["Approval Status"]}</td>
-            <td>${item["Payment Status"]}</td>
-            <td>${item["Report Date"]}</td>
-            <td>${item["Transaction Date"]}</td>
-            <td>${item["Total Approved Amount"]}</td>
-            <td>${item["City/Location"]}</td>
-            <td>${item["Payment Type"]}</td>
-            <td>${item["Approved Amount"]}</td>
-            <td>${item["Submit Date"]}</td>
-            <td>${item["Person Band before PMS"]}</td>
-            <td>${item["Location"]}</td>
-            <td>${item["Country/Region"]}</td>
+        <td>${item["Employee"]}</td>
+        <td>${item["Employee ID"]}</td>
+        <td>${item["Report Name"]}</td>
+        <td>${item["Expense Type"]}</td>
+        <td>${item["Report ID"]}</td>
+        <td>${item["Approval Status"]}</td>
+        <td>${item["Payment Status"]}</td>
+        <td>${item["Report Date"]}</td>
+        <td>${item["Transaction Date"]}</td>
+        <td>${item["Total Approved Amount"]}</td>
+        <td>${item["City/Location"]}</td>
+        <td>${item["Payment Type"]}</td>
+        <td>${item["Approved Amount"]}</td>
+        <td>${item["Submit Date"]}</td>
+        <td>${item["Employee Name"]}</td>
+        <td>${item["Report Start Date"]}</td>
+        <td>${item["Report End Date"]}</td>
+        <td>${item["Currency"]}</td>
+        <td>${item["Report Total"]}</td>
+        <td>${item["Receipt Status"]}</td>
+        <td>${item["Cash Advance Return Received"]}</td>
+        <td>${item["Amount Due Employee"]}</td>
+        <td>${item["Report Id"]}</td>
+        <td>${item["Approved by Delegate"]}</td>
+        <td>${item["Exception Approved"]}</td>
+        <td>${item["Expense Report Auditing Status"]}</td>
+        <td>${item["Has Cleared Exceptions"]}</td>
+        <td>${item["Person Band before PMS"]}</td>
+        <td>${item["Business Unit"]}</td>
+        <td>${item["Location"]}</td>
+        <td>${item["Gender"]}</td>
+        <td>${item["Logon ID"]}</td>
+        <td>${item["Email Address"]}</td>
+        <td>${item["Country/Region"]}</td>
+        <td>${item["Travel Rule Class"]}</td>
+        <td>${item["Employee_Manager"]}</td>
+        <td>${item["Employee ID_Manager"]}</td>
+        <td>${item["Report Name_Manager"]}</td>
+        <td>${item["Expense Type_Manager"]}</td>
+        <td>${item["Report ID_Manager"]}</td>
+        <td>${item["Approval Status_Manager"]}</td>
+        <td>${item["Payment Status_Manager"]}</td>
+        <td>${item["Report Date_Manager"]}</td>
+        <td>${item["Transaction Date_Manager"]}</td>
+        <td>${item["Total Approved Amount_Manager"]}</td>
+        <td>${item["City/Location_Manager"]}</td>
+        <td>${item["Payment Type_Manager"]}</td>
+        <td>${item["Approved Amount_Manager"]}</td>
+        <td>${item["Submit Date_Manager"]}</td>
+        <td>${item["Report Start Date_Manager"]}</td>
+        <td>${item["Report End Date_Manager"]}</td>
+        <td>${item["Currency_Manager"]}</td>
+        <td>${item["Report Total_Manager"]}</td>
+        <td>${item["Receipt Status_Manager"]}</td>
+        <td>${item["Cash Advance Return Received_Manager"]}</td>
+        <td>${item["Amount Due Employee_Manager"]}</td>
+        <td>${item["Report Id_Manager"]}</td>
+        <td>${item["Approved by Delegate_Manager"]}</td>
+        <td>${item["Exception Approved_Manager"]}</td>
+        <td>${item["Expense Report Auditing Status_Manager"]}</td>
+        <td>${item["Has Cleared Exceptions_Manager"]}</td>
+        <td>${item["Person Band before PMS_Manager"]}</td>
+        <td>${item["Business Unit_Manager"]}</td>
+        <td>${item["Location_Manager"]}</td>
+        <td>${item["Gender_Manager"]}</td>
+        <td>${item["Logon ID_Manager"]}</td>
+        <td>${item["Email Address_Manager"]}</td>
+        <td>${item["Country/Region_Manager"]}</td>
+        <td>${item["Default Expense Report Approver_Manager"]}</td>
+        <td>${item["Default Expense Report Approver ID_Manager"]}</td>
+        <td>${item["Default Cash Advance Approver_Manager"]}</td>
+        <td>${item["Default Cash Advance Approver ID_Manager"]}</td>
+        <td>${item["Default Authorization Request Approver_Manager"]}</td>
+        <td>${item["Default Authorization Request Approver ID_Manager"]}</td>
+        <td>${item["Default Travel Request Approver_Manager"]}</td>
+        <td>${item["Default Travel Request Approver ID_Manager"]}</td>
+        <td>${item["BI Manager_Manager"]}</td>
+        <td>${item["BI Manager ID_Manager"]}</td>
+        <td>${item["Travel Employee Manager_Manager"]}</td>
+        <td>${item["Travel Employee Manager ID_Manager"]}</td>
+        <td>${item["Travel Rule Class_Manager"]}</td>
+        <td>${item["Difference"]}</td>
         `
         searchTableBody.appendChild(row)
     })
